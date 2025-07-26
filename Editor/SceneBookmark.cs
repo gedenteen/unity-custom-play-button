0ceb7aa8c0ff97c375f390d26efd77581f715bdf
gedenteen <gedenteen@gmail.com>
2025-07-26 11:40:04 +0500

    Add button for SO SceneBookmark

--- Editor/SceneBookmark.cs
+++ Editor/SceneBookmark.cs
@@ -1,6 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 
 namespace ASze.CustomPlayButton
 {
@@ -16,6 +17,23 @@ namespace ASze.CustomPlayButton
         public void RemoveNullValue()
         {
             bookmarks.RemoveAll(item => item == null);
+        }
+        
+        // Adds the currently active scene to the list (if it isn't there yet)
+        public void AddCurrentScene()
+        {
+            var scene = EditorSceneManager.GetActiveScene();
+            if (!scene.IsValid()) return;
+
+            var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(scene.path);
+            if (sceneAsset == null) return;
+
+            if (!bookmarks.Contains(sceneAsset))
+            {
+                Undo.RecordObject(this, "Add Scene Bookmark");
+                bookmarks.Add(sceneAsset);
+                EditorUtility.SetDirty(this); // mark SO dirty for saving
+            }
         }
     }
 }
---
+++ Editor/SceneBookmarkEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ASze.CustomPlayButton
+{
+    // For buttons in Inspector (for SO SceneBookmark)
+    [CustomEditor(typeof(SceneBookmark))]
+    public class SceneBookmarkEditor : Editor
+    {
+        private GUIStyle _buttonStyle;
+
+        private void InitStyles()
+        {
+            if (_buttonStyle != null) return;
+
+            _buttonStyle = new GUIStyle(GUI.skin.button)
+            {
+                fixedHeight = 30,
+                fontSize = 14,
+                fontStyle = FontStyle.Bold
+            };
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            GUILayout.Space(10);
+
+            InitStyles();
+
+            if (GUILayout.Button("Add Current Scene", _buttonStyle))
+            {
+                var bookmark = (SceneBookmark)target;
+                bookmark.AddCurrentScene();
+            }
+        }
+    }
+}
